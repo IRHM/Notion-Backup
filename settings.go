75a0144055bdd4217ec918a34e3a198d43c2f329
IRHM <37304121+IRHM@users.noreply.github.com>
2020-08-01 14:40:27 +0100

    Re-read settings after writing them
    And prevent getSettingsFile being run multiple times when it doesn't need to be.

--- settings.go
+++ settings.go
@@ -13,7 +13,7 @@ type settings struct {
 }
 
 func getSettings() error {
-	err := readSettings()
+	err := readSettings(getSettingsFile())
 	if err != nil {
 		return errors.New("Error getting settings: " + err.Error())
 	}
@@ -21,38 +21,29 @@ func getSettings() error {
 	return nil
 }
 
-func getSettingsFile() string {
-	var settingsFile = "settings.json"
-
-	// Create settingsFile if it doesn't already exist
-	if _, err := os.Stat(settingsFile); os.IsNotExist(err) {
-		fmt.Println("Settings file not found, creating it.")
-		os.Create(settingsFile)
-	} else {
-		fmt.Println("Found settings file.")
-	}
-
-	return settingsFile
-}
-
-func readSettings() error {
+func readSettings(file string) error {
 	// Read settings file
-	data, err := ioutil.ReadFile(getSettingsFile())
+	data, err := ioutil.ReadFile(file)
 	if err != nil {
 		return err
 	} else if len(data) < 1 {
 		// If settings file is empty then ask user for settings and write them
-		writeSettings()
+		writeSettings(file)
+
+		// Re-read settings
+		readSettings(file)
 	}
 
 	// Deserialize json settings
 	end := settings{}
 	json.Unmarshal(data, &end)
 
+	fmt.Println(end.APITokenV2)
+
 	return nil
 }
 
-func writeSettings() error {
+func writeSettings(file string) error {
 	// Get user input for api token
 	fmt.Println("API token_v2: ")
 	var APITokenV2Input string
@@ -72,4 +63,18 @@ func writeSettings() error {
 	ioutil.WriteFile(getSettingsFile(), settingsJSON, 0644)
 
 	return nil
+}
+
+func getSettingsFile() string {
+	var settingsFile = "settings.json"
+
+	// Create settingsFile if it doesn't already exist
+	if _, err := os.Stat(settingsFile); os.IsNotExist(err) {
+		fmt.Println("Settings file not found, creating it.")
+		os.Create(settingsFile)
+	} else {
+		fmt.Println("Found settings file.")
+	}
+
+	return settingsFile
 }
