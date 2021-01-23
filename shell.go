a68629e3cb82133e6249f99219e5099909ccfd46
kingo2157 <kingo2157@protonmail.com>
2021-01-23 22:22:35 +0000

    Linux fix for run command
    Added prefix var

--- shell.go
+++ shell.go
@@ -7,13 +7,15 @@ import (
 
 func runCommand(command string, dir string, stdin bool) (string, error) {
 	shell := "/bin/sh"
+	prefix := "-"
 
 	// If on windows change shell to powershell
 	if runtime.GOOS == "windows" {
 		shell = "powershell"
+		prefix = "/"
 	}
 
-	cmd := exec.Command(shell, "/c", command)
+	cmd := exec.Command(shell, prefix+"c", command)
 	cmd.Dir = dir
 
 	if stdin {
